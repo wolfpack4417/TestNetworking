fdad9517765777b21aae44650a8285eb233dbf0f
Tucker Peebles <tucker@bldg-25.com>
2016-06-13 17:30:39 -0400

    BAD COMMIT: updated PlayerMove script to spawn the local player as red and updated script so moving will only control the local player

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,10 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class PlayerMove : MonoBehaviour {
+public class PlayerMove : NetworkBehaviour {
+
+	public override void OnStartLocalPlayer()
+	{
+		GetComponent<MeshRenderer>().material.color = Color.red;
+	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (!isLocalPlayer) 
+		{
+			return;
+		}
+
 		var x = Input.GetAxis("Horizontal")*0.1f;
 		var z = Input.GetAxis("Vertical")*0.1f;
 
